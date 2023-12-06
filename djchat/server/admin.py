4f12bd97f8bc7c5c9b0522e08bcb8a2a1d2a14f9
Adnan <adbak.dev@gmail.com>
2023-12-06 23:56:40 +0100

    admin

--- djchat/server/admin.py
+++ djchat/server/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Channel, Server, Category
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+
+
 admin.site.register(Channel)
 admin.site.register(Server)
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
