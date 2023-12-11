115cdaadd627fdb007ae8244d390557e2feeaf7a
Adnan <adbak.dev@gmail.com>
2023-12-11 23:09:05 +0100

    drawer

--- reactchat/src/pages/templates/PrimaryAppBar.tsx
+++ reactchat/src/pages/templates/PrimaryAppBar.tsx
@@ -1,4 +1,13 @@
-import { AppBar, Link, Toolbar, Typography, useTheme } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const PrimaryAppBar = () => {
   const theme = useTheme();
@@ -16,6 +25,20 @@ const PrimaryAppBar = () => {
           minHeight: theme.primaryAppBar.height,
         }}
       >
+        <Box
+          sx={{
+            display: { xs: "block", sm: "none" },
+          }}
+        >
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Box>
         <Link href="/" underline="none" color="inherit">
           <Typography
             variant="h6"
