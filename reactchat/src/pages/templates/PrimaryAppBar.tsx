533461149a0bb2eb68563afff3b52e45b06c45c5
Adnan <adbak.dev@gmail.com>
2023-12-11 23:42:45 +0100

    drawer

--- reactchat/src/pages/templates/PrimaryAppBar.tsx
+++ reactchat/src/pages/templates/PrimaryAppBar.tsx
@@ -1,6 +1,7 @@
 import {
   AppBar,
   Box,
+  Drawer,
   IconButton,
   Link,
   Toolbar,
@@ -8,12 +9,19 @@ import {
   useTheme,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
 
 const PrimaryAppBar = () => {
+  const [sideMenu, setSideMenu] = useState(false);
   const theme = useTheme();
+
+  const toggleDrawer = (open: boolean) => {
+    setSideMenu(!open);
+  };
   return (
     <AppBar
       sx={{
+        zIndex: (theme) => theme.zIndex.drawer + 1,
         backgroundColor: theme.palette.background.default,
         borderBottom: `1px solid ${theme.palette.divider}`,
       }}
@@ -35,10 +43,27 @@ const PrimaryAppBar = () => {
             aria-label="open drawer"
             edge="start"
             sx={{ mr: 2 }}
+            onClick={() => toggleDrawer(sideMenu)}
           >
             <MenuIcon />
           </IconButton>
         </Box>
+        <Drawer anchor="left" open={sideMenu}>
+          {[...Array(100)].map((_, index) => (
+            <Box
+              key={index}
+              sx={{
+                width: 250,
+                height: "100%",
+                backgroundColor: theme.palette.background.paper,
+              }}
+            >
+              <Typography variant="h6" component="div" sx={{ p: 2 }}>
+                Menu
+              </Typography>
+            </Box>
+          ))}
+        </Drawer>
         <Link href="/" underline="none" color="inherit">
           <Typography
             variant="h6"
