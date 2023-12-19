4cc289684e267009365a6944f1f1efc32dee50ac
Adnan <adbak.dev@gmail.com>
2023-12-19 23:22:18 +0100

    jwt interceptor

---
+++ reactchat/src/helpers/jwtinterceptor.ts
@@ -0,0 +1,27 @@
+import { BASE_URL } from "./../../config";
+import axios, { AxiosInstance } from "axios";
+import { useNavigate } from "react-router-dom";
+
+const API_BASE_URL = BASE_URL;
+
+const useAxiosWithInterceptor = (): AxiosInstance => {
+  const jwtAxios = axios.create({ baseURL: API_BASE_URL });
+  const navigate = useNavigate();
+
+  jwtAxios.interceptors.request.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      const originalRequest = error.config;
+      if (error.response?.status === 401) {
+        const goRoot = () => navigate("/");
+        goRoot();
+      }
+      throw error;
+    }
+  );
+  return jwtAxios;
+};
+
+export default useAxiosWithInterceptor;
